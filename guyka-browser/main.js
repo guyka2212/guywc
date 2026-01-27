5f51a565463af461f9c8e14039afcc82e12678d0
guyka2212 <2212kadosh@gmail.com>
2026-01-27 20:52:25 +0200

    added

--- guyka-browser/main.js
+++ guyka-browser/main.js
@@ -1,39 +1,19 @@
-const { app, BrowserWindow } = require('electron')
-const https = require('https')
-
-let registry = {}
-
 function loadRegistry(cb) {
   https.get(
-    'https://raw.githubusercontent.com/USERNAME/guyka-registry/main/sites.json',
+    'https://raw.githubusercontent.com/guyka2212/guyka-registry/main/sites.json',
     res => {
       let data = ''
       res.on('data', d => data += d)
       res.on('end', () => {
-        registry = JSON.parse(data)
-        cb()
+        try {
+          registry = JSON.parse(data)
+          cb()
+        } catch (e) {
+          console.error('JSON parse error:', e)
+          registry = {}
+          cb()
+        }
       })
     }
   )
 }
-
-app.whenReady().then(() => {
-  loadRegistry(() => {
-    const win = new BrowserWindow({
-      width: 1200,
-      height: 800
-    })
-
-    win.loadURL('https://google.com')
-
-    win.webContents.on('will-navigate', (e, url) => {
-      if (url.startsWith('guyka://')) {
-        e.preventDefault()
-        const name = url.replace('guyka://', '')
-        if (registry[name]) {
-          win.loadURL(registry[name])
-        }
-      }
-    })
-  })
-})
